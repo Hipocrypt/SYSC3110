467da254772cc2a10c6b0a850ee8bdb5b513ddf3
Daniel Esenwa <dansnwa@@gmail.com>
2023-09-18 13:15:12 -0400

    added a print function that prints out the string, 'Address Book'

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,10 +1,21 @@
+import java.util.ArrayList;
+
 public class AddressBook {
-    public void addBuddy() {
-    }
-    public void removeBuddy() {
+
+    ArrayList<BuddyInfo> myList = new ArrayList<>();
+
+    public void addBuddy(BuddyInfo buddyinfo) {
+        myList.add(buddyinfo);
     }
 
-    public static void main(String[] args){
-        System.out.println("Address Book");
+    public void removeBuddy(BuddyInfo buddyinfo) {
+        myList.remove(buddyinfo);
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Daniel Esenwa", "Carleton", "99");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
--- src/BuddyInfo.java
+++ src/BuddyInfo.java
@@ -1,30 +1,33 @@
 public class BuddyInfo {
-   private String name;
+    private final String name;
+    private String address;
+    private String number;
+
     public BuddyInfo() {
         this.name = "Daniel Esenwa";
     }
 
-    public BuddyInfo(String name) {
-
+    public BuddyInfo(String name, String address, String number) {
         this.name = name;
+        this.address = address;
+        this.number = number;
     }
 
     public String getName() {
         return this.name;
-
-
-
-
+    }
+    public String getAddress() {
+        return this.address;
     }
 
-
+    public String getNumber() {
+        return this.number;
+    }
 
 
     public static void main(String[] args) {
         BuddyInfo hall = new BuddyInfo();
-        System.out.println("Hello, " + hall.getName());
-        BuddyInfo mark = new BuddyInfo("joshua");
-        System.out.println("Hello, " + mark.getName());
+        System.out.println("Hello, " + hall.getName() + hall.getAddress() + hall.getNumber() );
     }
 }
 
