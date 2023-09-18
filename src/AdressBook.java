f71fc4173c7622177914da9f43a9d1ac472bb2b9
Daniel Esenwa <dansnwa@@gmail.com>
2023-09-18 12:02:02 -0400

    added a print function that prints out the string, 'Address Book'

---
+++ src/AdressBook.java
@@ -0,0 +1,10 @@
+public class AdressBook {
+    public void addBuddy() {
+    }
+    public void removeBuddy() {
+    }
+
+    public static void main(String[] args){
+        System.out.println("Address Book");
+    }
+}
